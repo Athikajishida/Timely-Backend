94089599b536e1af0e757eb6bec3759cfb6b5051
Athika Jishida <athikajishida@gmail.com>
2024-11-21 14:10:36 +0530

    production commit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   post '/login', to: 'sessions#create'
   post '/signup', to: 'registrations#create'
   post '/confirm_email', to: 'registrations#confirm_email' # For OTP confirmation
